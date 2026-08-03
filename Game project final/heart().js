e6b7c1f94800d1844ffd5c747366ac3463b445e3
K_Anand <86229854+K-A05@users.noreply.github.com>
2026-08-03 17:12:48 +0100

    Delete Game project final directory

--- Game project final/heart().js
+++
@@ -1,8 +0,0 @@
-function heart(x, y, size)
-	{
-		beginShape();
-		vertex(x, y - size / 4);
-		bezierVertex(x + size / 2, y - size / 2, x + size / 2, y + size / 4, x, y + size / 2);
-		bezierVertex(x - size / 2, y + size / 4, x - size / 2, y - size / 2, x, y - size / 4);
-		endShape();
-	}
